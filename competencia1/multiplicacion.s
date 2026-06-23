661204e0a308ef006206afe547a7109acc33f48a
Luis Gerardo López Ruiz <gearstaker5@gmail.com>
2026-06-23 17:47:42 -0600

    Competencia 1 | fase de generación de código: se crean los 4 códigos de operaciones básicas con su compilación en ensamblador

---
+++ competencia1/multiplicacion.s
@@ -0,0 +1,21 @@
+	.file	"multiplicacion.c"
+	.text
+	.globl	func
+	.def	func;	.scl	2;	.type	32;	.endef
+	.seh_proc	func
+func:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	16(%rbp), %eax
+	imull	24(%rbp), %eax
+	movl	%eax, 16(%rbp)
+	movl	16(%rbp), %eax
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev5, Built by MSYS2 project) 16.1.0"
---
+++ competencia1/resta.s
@@ -0,0 +1,20 @@
+	.file	"resta.c"
+	.text
+	.globl	func
+	.def	func;	.scl	2;	.type	32;	.endef
+	.seh_proc	func
+func:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	24(%rbp), %eax
+	subl	%eax, 16(%rbp)
+	movl	16(%rbp), %eax
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev5, Built by MSYS2 project) 16.1.0"
